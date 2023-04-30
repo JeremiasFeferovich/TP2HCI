1418bb1ccfe3aeb3b6f357a3a20f361ad8b78540
Agustin Andres Gutierrez <agutierrez@itba.edu.ar>
2023-04-30 15:13:15 -0300

    Added device cards

--- intelicasa/src/views/DevicesView.vue
+++ intelicasa/src/views/DevicesView.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Devices</h1>
+    <v-container fluid>
+        <v-row no-gutters>
+            <v-col v-for="n in 5" :key="n" cols="12" sm="6" lg="3">
+                <DeviceCard />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<script setup>
+import DeviceCard from '@/components/DeviceCard.vue';
+
+</script>
