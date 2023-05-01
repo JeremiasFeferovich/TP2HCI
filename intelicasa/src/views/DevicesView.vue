d82e8e8e0c95b2a74e584a630a03f6bade72a74f
JeremiasFeferovich <95632444+JeremiasFeferovich@users.noreply.github.com>
2023-05-01 11:10:48 -0300

    searchbar in devices page implemented

--- intelicasa/src/views/DevicesView.vue
+++ intelicasa/src/views/DevicesView.vue
@@ -2,6 +2,7 @@
     <TitleComponent title="Devices"/>
     
     <v-container fluid >
+        <input v-model.trim="search" type="text" placeholder="Search">
         <v-row no-gutters>
             <v-col v-for="(device, index) in devices" :key="index" cols="12" sm="6" lg="3">
                 <DeviceCard :title="device.title"/>
@@ -13,13 +14,22 @@
 <script setup>
     import TitleComponent from '@/components/TitleComponent.vue';
     import DeviceCard from '@/components/DeviceCard.vue';
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
 
-    const devices = ref([{title: "disp 1"}, 
+    const search = ref("")
+    
+    const d = [{title: "disp 1"}, 
                         {title: "disp 2"}, 
                         {title: "disp 3"}, 
                         {title: "disp 4"},
-                        {title: "disp 5"}]);
+                        {title: "disp 5"}];
+    const devices = ref(d)
+        
+
+
+    watch(search, () => {
+    devices.value = d.filter(device => device.title.toLowerCase().includes(search.value.toLowerCase()))
+    })
 
 </script>
 
