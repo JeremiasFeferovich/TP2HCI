483e88e228c15125ed47180ef42c4c8560cfd45f
Agustin Andres Gutierrez <agutierrez@itba.edu.ar>
2023-05-01 15:04:37 -0300

    Added task extra info

--- intelicasa/src/components/DeviceCard.vue
+++ intelicasa/src/components/DeviceCard.vue
@@ -1,60 +1,68 @@
 <template>
-    <v-card class="ma-2 pa-3">
+    <v-card class="ma-2 pa-3" @click.stop="openDialog = true">
         <v-row align="center">
             <v-col cols="3">
                 <img :src="categoryImg" alt="categoryImg" style="max-height: 100%; max-width: 100%;" />
             </v-col>
             <v-col cols="6" class="text-center" align-self="center">
-                <v-card-title>{{ deviceTitle }}</v-card-title>
+                <v-card-title>{{ device.name }}</v-card-title>
             </v-col>
             <v-col cols="3">
-                <v-btn v-model="buttonState" @click="toggleButtonState" toggle :ripple="false" size="large" variant="plain" :loading="loading" rounded="xl">
+                <v-btn v-model="device.isOn" @click.stop="toggleButtonState" toggle :ripple="false" size="large"
+                    variant="plain" :loading="loadingState" rounded="xl">
                     <img :src="powerBtnImg" alt="powerState" />
                 </v-btn>
             </v-col>
         </v-row>
+        <v-dialog v-model="openDialog" width="30%">
+            <LightDeviceInfo :device="device" :loadingState="loadingState" :toggleButtonState="toggleButtonState"/>
+        </v-dialog>
     </v-card>
 </template>
-
+  
 <script setup>
-
 import { ref, computed, defineProps } from 'vue';
 import powerOn from '@/assets/powerOn.svg';
 import powerOff from '@/assets/powerOff.svg'
 import lightbulb from '@/assets/lightbulb.svg'
+import LightDeviceInfo from './LightDeviceInfo.vue';
 
-    const buttonState = ref(false);
-    const loading = ref(false);
-    
-    const powerBtnImg = computed(() => {
-        return buttonState.value ? powerOn : powerOff;
-    })
+const loadingState = ref(false);
+const openDialog = ref(false);
 
-    const categoryImg = lightbulb;
+const { device } = defineProps({
+    device: Object
+})
 
-    function toggleButtonState() {
-        loading.value = true
-        setTimeout(() => (loading.value = false), 1000)
-        buttonState.value = !buttonState.value;
-    }
+const categoryImg = lightbulb;
 
-    const prop = defineProps({
-        deviceTitle: String
-    })
+const powerBtnImg = computed(() => {
+    return device.isOn ? powerOn : powerOff;
+})
+
+function toggleButtonState() {
+    loadingState.value = true
+    setTimeout(() => (loadingState.value = false), 1000)
+    device.isOn = !device.isOn;
+}
+
 
 </script>
-
+  
 <style scoped>
-    .v-row {
-        height: 100%;
-    }
-    .v-btn {
-        display: flex; 
-        justify-content: center; 
-        align-items: center;
-    }
-    .v-btn img {
-        max-height: 100%; 
-        max-width: 100%;
-    }
+.v-row {
+    height: 100%;
+}
+
+.v-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.v-btn img {
+    max-height: 100%;
+    max-width: 100%;
+}
 </style>
+  
