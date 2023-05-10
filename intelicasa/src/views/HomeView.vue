ce8147d1f6938fb24af1eeb3fc0097b5f4f2d641
JeremiasFeferovich <95632444+JeremiasFeferovich@users.noreply.github.com>
2023-05-10 20:10:50 -0300

    Favorite routines added to homeView

--- intelicasa/src/views/HomeView.vue
+++ intelicasa/src/views/HomeView.vue
@@ -1,17 +1,35 @@
 <template>
     <TitleComponent title="Inicio" class="mb-5" />
     <v-container fluid>
-        <v-row justify="center">
-            <h1 class="text-h4">Rutinas</h1>
-        </v-row>
         <v-sheet class="viewSheet" color="secondary">
             <v-row justify="center" class="mb-5 text-h4">
-                Dispositivos
-                <v-divider/>
+                Rutinas destacadas
+                <v-divider />
             </v-row>
-            <p class="text-h6" align="center" v-if="deviceStore.devices.length && !deviceStore.devices.filter(device => device.meta.favorite).length">Aún no tienes dispositivos favoritos</p>
-            <DevicesList v-if="!loading" :showSearchbar="false" :devices="deviceStore.devices.filter(device => device.meta.favorite)"
-                @delete="(device) => deleteDevice(device)"/>
+            <p class="text-h6" align="center"
+                v-if="routineStore.routines.length && !routineStore.routines.filter(routine => routine.meta.favorite).length">
+                Aún no tienes rutinas favoritos</p>
+            <v-row v-if="!loading"
+                v-for="(routine, index) in routineStore.routines.filter(routine => routine.meta.favorite)"
+                :key="routine.id">
+                <RoutineCard :name="routine.name" :routine="routine" @delete-routine="deleteRoutine(routine)"
+                    :allDevices="deviceStore.devices" />
+            </v-row>
+            <DevicesList v-if="!loading" :showSearchbar="false"
+                :devices="deviceStore.devices.filter(device => device.meta.favorite)"
+                @delete="(device) => deleteDevice(device)" />
+        </v-sheet>
+        <v-sheet class="viewSheet" color="secondary">
+            <v-row justify="center" class="mb-5 text-h4">
+                Dispositivos destacados
+                <v-divider />
+            </v-row>
+            <p class="text-h6" align="center"
+                v-if="deviceStore.devices.length && !deviceStore.devices.filter(device => device.meta.favorite).length">Aún
+                no tienes dispositivos favoritos</p>
+            <DevicesList v-if="!loading" :showSearchbar="false"
+                :devices="deviceStore.devices.filter(device => device.meta.favorite)"
+                @delete="(device) => deleteDevice(device)" />
         </v-sheet>
     </v-container>
 </template>
@@ -23,14 +41,18 @@ import { ref } from 'vue';
 import DevicesList from '@/components/devices/DevicesList.vue';
 import { useDeviceStore } from '@/stores/deviceStore';
 import { onMounted } from 'vue';
+import { useRoutineStore } from '@/stores/routineStore';
+import RoutineCard from '@/components/RoutineCard.vue';
 
 const loading = ref(false)
 const deviceStore = useDeviceStore();
+const routineStore = useRoutineStore();
 
 onMounted(async () => {
     loading.value = true
     await deviceStore.fetchDevices();
     await deviceStore.fetchCategories();
+    await routineStore.fetchRoutines();
     loading.value = false
 })
 
@@ -38,6 +60,9 @@ function deleteDevice(device) {
     deviceStore.deleteDevice(device)
 }
 
+function deleteRoutine(routine) {
+    routineStore.deleteRoutine(routine)
+}
 </script>
 
 <style scoped>
